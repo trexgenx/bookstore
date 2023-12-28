c3e621ca095fbe9e95f463075b93b873f475c681
trexgenx <trexgenx@gmail.com>
2023-12-28 10:11:48 -0800

    initial commit

--- models/book.js
+++ models/book.js
@@ -38,22 +38,45 @@ const bookSchema = mongoose.Schema({
 const Book = module.exports = mongoose.model('Book', bookSchema);
 
 // Get Books
-module.exports.getBooks = (callback, limit) => {
+/*module.exports.getBooks = (callback, limit) => {
 	Book.find(callback).limit(limit);
-}
+}*/
+module.exports.getBooks = (limit) => {
+	return new Promise((resolve, reject) => {
+	  Book.find()
+		.limit(limit)
+		.exec()
+		.then(books => resolve(books))
+		.catch(err => reject(err));
+	});}
 
 // Get Book
-module.exports.getBookById = (id, callback) => {
+/*module.exports.getBookById = (id, callback) => {
 	Book.findById(id, callback);
-}
+}*/
+
+module.exports.getBookById = (id) => {
+	return new Promise((resolve, reject) => {
+	  Book.findById(id)
+		.exec()
+		.then(book => resolve(book)) 
+		.catch(err => reject(err));
+	});}
 
 // Add Book
-module.exports.addBook = (book, callback) => {
+/*module.exports.addBook = (book, callback) => {
 	Book.create(book, callback);
-}
+}*/
+module.exports.addBook = (book) => {
+	return new Promise((resolve, reject) => {
+	  Book.create(book)
+		.then(addedBook => resolve(addedBook))
+		.catch(err => reject(err));
+	}); 
+  }
 
 // Update Book
-module.exports.updateBook = (id, book, options, callback) => {
+/*module.exports.updateBook = (id, book, options, callback) => {
 	var query = {_id: id};
 	var update = {
 		title: book.title,
@@ -66,10 +89,28 @@ module.exports.updateBook = (id, book, options, callback) => {
 		buy_url: book.buy_url
 	}
 	Book.findOneAndUpdate(query, update, options, callback);
-}
+}*/
+module.exports.updateBook = (id, book) => {
+	return new Promise((resolve, reject) => {
+	  Book.findOneAndUpdate(
+		{ _id: id }, 
+		book,
+		{ new: true } 
+	  )
+	  .then(updatedBook => resolve(updatedBook))
+	  .catch(err => reject(err));
+	});
+  }
 
 // Delete Book
-module.exports.removeBook = (id, callback) => {
+/*module.exports.removeBook = (id, callback) => {
 	var query = {_id: id};
 	Book.remove(query, callback);
-}
+}*/
+module.exports.removeBook = (id) => {
+	return new Promise((resolve, reject) => {
+	  Book.deleteOne({_id: id })
+		.then(result => resolve(result)) 
+		.catch(err => reject(err));
+	});
+  }
--- models/genre.js
+++ models/genre.js
@@ -15,27 +15,62 @@ const genreSchema = mongoose.Schema({
 const Genre = module.exports = mongoose.model('Genre', genreSchema);
 
 // Get Genres
-module.exports.getGenres = (callback, limit) => {
+/*module.exports.getGenres = (callback, limit) => {
 	Genre.find(callback).limit(limit);
-}
+}*/
+module.exports.getGenres = (limit) => {
+	return new Promise((resolve, reject) => {
+	  Genre.find()
+		.limit(limit)
+		.exec()
+		.then(genres => resolve(genres)) 
+		.catch(err => reject(err));
+	});
+  }
 
 // Add Genre
-module.exports.addGenre = (genre, callback) => {
+/*module.exports.addGenre = (genre, callback) => {
 	Genre.create(genre, callback);
-}
+}*/
+
+module.exports.addGenre = (genre) => {
+	return new Promise((resolve, reject) => {
+	  Genre.create(genre)
+		.then(addedGenre => resolve(addedGenre))
+		.catch(err => reject(err));
+	});
+  }
 
 // Update Genre
-module.exports.updateGenre = (id, genre, options, callback) => {
+/*module.exports.updateGenre = (id, genre, options, callback) => {
 	var query = {_id: id};
 	var update = {
 		name: genre.name
 	}
 	Genre.findOneAndUpdate(query, update, options, callback);
-}
+}*/
+module.exports.updateGenre = (id, updatedGenre) => {
+	return new Promise((resolve, reject) => {
+	  Genre.findOneAndUpdate(
+		{ _id: id },
+		updatedGenre, 
+		{ new: true }
+	  )
+	  .then(genre => resolve(genre))
+	  .catch(err => reject(err));
+	});
+  }
 
 
 // Delete Genre
-module.exports.removeGenre = (id, callback) => {
+/*module.exports.removeGenre = (id, callback) => {
 	var query = {_id: id};
 	Genre.remove(query, callback);
-}
+}*/
+module.exports.removeGenre = (id) => {
+	return new Promise((resolve, reject) => {
+	  Genre.deleteOne({ _id: id})
+		.then(result => resolve(result))
+		.catch(err => reject(err));
+	}); 
+  }
